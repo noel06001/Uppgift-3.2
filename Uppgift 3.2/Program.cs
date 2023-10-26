7870cff14aaae3f276c3b65de8bc297c632a1cd8
noell <noell@DESKTOP-NLQ10I7>
2023-10-26 17:12:18 +0200

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Upgift_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? Svara antingen \"j\" för ja eller \"n\" för nej");
+            string x = Console.ReadLine().ToLower();
+           
+            Console.WriteLine("Hur gammal är du?");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x=="j" && y<22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu");
+            }
+            Console.ReadKey();
+        }
+    }
+}
